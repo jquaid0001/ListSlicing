17163b486a2a4cf7a899465802d8474d11df5e25
Josh Quaid <josh.quaid@gmail.com>
2022-08-15 13:10:44 -0500

    Final Product

---
+++ src/Solution.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY s
+     *  2. INTEGER d
+     *  3. INTEGER m
+     */
+
+    public static int birthday(List<Integer> s, int d, int m) {
+        int index = 0;
+        int numSegments = 0;
+
+        while(index <= (s.size() - m)) {
+            // If the slice sums to d increment numSegments
+            if(s.subList(index, (m + index)).stream().reduce(0, Integer::sum) == d) {
+                numSegments++;
+            }
+            index++;
+        }
+
+        return numSegments;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> s = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int d = Integer.parseInt(firstMultipleInput[0]);
+
+        int m = Integer.parseInt(firstMultipleInput[1]);
+
+        int result = Result.birthday(s, d, m);
+        System.out.println(result);
+
+        bufferedReader.close();
+
+    }
+}
